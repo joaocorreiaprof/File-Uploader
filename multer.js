99e90194cbe9c8f4e8aa5b2ec8fe960a78111633
João Correia <joaocorreiaprof@gmail.com>
2025-01-20 19:43:32 +0000

    updated json

--- multer.js
+++ multer.js
@@ -4,13 +4,13 @@ const path = require("path");
 // Configure multer for file upload
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/"); // Temporary directory for file storage
+    cb(null, "uploads/");
   },
   filename: (req, file, cb) => {
-    cb(null, Date.now() + "-" + file.originalname); // Use a unique filename
+    cb(null, Date.now() + "-" + file.originalname);
   },
 });
 
-const upload = multer({ storage: storage }).single("file"); // 'file' matches the form field name
+const upload = multer({ storage: storage }).single("file");
 
 module.exports = upload;
