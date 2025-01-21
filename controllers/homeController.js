752451e0c96cab125accf7a36ec5ed68f371f631
João Correia <joaocorreiaprof@gmail.com>
2025-01-21 12:52:49 +0000

    Fixed duplicated code in home route

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,18 @@
+const crudController = require("../controllers/crudController");
+
 module.exports = {
   getHomePage: (req, res) => {
-    res.render("index", { user: req.user });
+    if (!req.user) {
+      return res.redirect("/log-in");
+    }
+
+    // Fetch user folders and render the page
+    crudController.getUserFolders(req, res, (err, folders) => {
+      if (err) {
+        return res.status(500).send("Error fetching folders");
+      }
+
+      res.render("index", { user: req.user, folders: folders });
+    });
   },
 };
--- routes/homeRoutes.js
+++ routes/homeRoutes.js
@@ -1,22 +1,8 @@
-// routes/homeRoutes.js
 const express = require("express");
 const router = express.Router();
-const crudController = require("../controllers/crudController");
+const homeController = require("../controllers/homeController");
 
-router.get("/", (req, res) => {
-  if (!req.user) {
-    return res.redirect("/log-in"); // Redirect to login if the user is not authenticated
-  }
-
-  // Fetch user folders and render the page
-  crudController.getUserFolders(req, res, (err, folders) => {
-    if (err) {
-      return res.status(500).send("Error fetching folders");
-    }
-
-    // Render the index page with the user and their folders
-    res.render("index", { user: req.user, folders: folders });
-  });
-});
+// Home route
+router.get("/", homeController.getHomePage);
 
 module.exports = router;
